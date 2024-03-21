0f48a63048741faa96acfa663f013c142b071786
n-freman <smiron1703@gmail.com>
2024-03-21 11:36:44 +0500

    finish chem_parser

--- src/chem_parser.py
+++ src/chem_parser.py
@@ -1,5 +1,5 @@
 import json
-import re
+from typing import Dict, List, Literal
 
 with open('data.json') as file:
     elements = json.load(file)
@@ -11,21 +11,62 @@ class ChemParser:
     '''
     elements = elements
     titles = elements.keys()
-    regex = f'^({"|".join(map(lambda x: x.lower(), titles))})+$'
 
     @classmethod
-    def dispatch(cls, text: str) -> bool:
-        return cls.search(cls.clean(text))
+    def dispatch(cls, text: str) -> List[List[str]]:
+        if len(text) == 0:
+            raise EmptyTextError(
+                'Can not find elements in empty text'
+            )
+        return cls._find_full_sequences(cls._search(cls._clean(text)))
 
     @classmethod
-    def search(cls, text: str) -> bool:
-        match = re.match(cls.regex, text)
-        return bool(match)
+    def _find_full_sequences(cls, results: Dict[str, Dict] | Literal[True]) -> List[List[str]]:
+    # Iterate over results
+        if results is True:
+            return [['']]
+        if results == {}:
+            return False
+        combinations = []
+        print(results)
+        for key in results.keys():
+            result = cls._find_full_sequences(results[key])
+            if not result:
+                continue
+            combinations.extend(
+                cls.merge_results(
+                    key,
+                    cls._find_full_sequences(results[key])
+                )
+            )
+        return combinations
 
     @classmethod
-    def clean(cls, text: str) -> str:
+    def _search(cls, text: str) -> Dict[str, Dict] | Literal[True]:
+        if len(text) == 0:
+            return True
+        result = {}
+        for el in cls.titles:
+            if text.startswith(el):
+                result[el] = cls._search(text[len(el):])
+        return result
+
+
+    @classmethod
+    def _clean(cls, text: str) -> str:
         clean_text = ''
         for ch in text:
             if ch.isalpha(): clean_text += ch.lower()
         return clean_text
+
+    @staticmethod
+    def merge_results(element, combinations):
+        result = []
+        for i in combinations:
+            result.append(element + i)
+        return result
+
+
+class EmptyTextError(Exception):
+    pass
 
